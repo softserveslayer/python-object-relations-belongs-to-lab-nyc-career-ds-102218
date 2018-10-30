a1d93307ff0b6f766a2f5a85500d4f5c0d5326ba
Jaime Cheng <Clementine@Clementine.local>
2018-10-30 18:08:15 -0400

    Final

--- car.py
+++ car.py
@@ -8,3 +8,44 @@ class Car:
         self._year = year
         self._owner = owner
         Car._all.append(self)
+
+    @property
+    def make(self):
+        return self._make
+
+    @make.setter
+    def make(self, make):
+        self._make = make
+
+    @property
+    def model(self):
+        return self._model
+
+    @model.setter
+    def model(self, model):
+        self._model = model
+
+    @property
+    def year(self):
+        return self._year
+
+    @year.setter
+    def year(self, year):
+        self._year = year
+
+    @property
+    def owner(self):
+        return self._owner
+
+    @owner.setter
+    def owner(self, owner):
+        self._owner = owner
+
+    @classmethod
+    def all(cls):
+        return Car._all
+
+
+    @classmethod
+    def cars_driven_by(cls, occupation):
+        return [car.owner.name for car in Car._all if car.owner.occupation == occupation]
--- person.py
+++ person.py
@@ -2,7 +2,31 @@
 from car import Car
 
 class Person:
-
     def __init__(self, name, occupation):
         self._name = name
         self._occupation = occupation
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def occupation(self):
+        return self._occupation
+
+    @classmethod
+    def has_oldest_car(cls):
+        cars = Car._all
+        dict = {}
+        for car in Car._all:
+            dict[car.owner.name] = car.year
+        return min(dict, key=dict.get)
+
+    @classmethod
+    def drives_a(cls, car_name):
+        return [car.owner.name for car in Car._all if car.make == car_name]
+
+
+    def drives_same_make_as_me(self):
+        my_car = list(filter(lambda car: car.owner == self, Car._all))
+        return [car.owner.name for car in Car._all if car.make == my_car[0].make and car.owner != my_car[0].owner]
